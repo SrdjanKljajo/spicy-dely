8da842f6d172f9720bdac84c4a473044496949ac
SrdjanKljajo <websmartist@gmail.com>
2021-12-02 00:17:47 +0100

    create error handler

---
+++ errors/index.js
@@ -0,0 +1,10 @@
+import CustomAPIError from './custom-api.js'
+import NotFoundError from './not-found.js'
+import BadRequestError from './bad-request.js'
+import ServerError from './server-error.js'
+export default {
+  CustomAPIError,
+  NotFoundError,
+  BadRequestError,
+  ServerError,
+}
